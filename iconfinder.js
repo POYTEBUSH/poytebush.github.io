732f6401f15d243fb69e520e082e1505d642bd75
Peter Bush <poytebush@gmail.com>
2018-12-20 22:46:06 +0000

    Updated website

---
+++ iconfinder.js
@@ -0,0 +1,14 @@
+function GetIcon(text){
+    if(text === "c++"){
+        return '<img src="https://img.icons8.com/color/50/000000/c-plus-plus-logo.png" class="img-fluid">'
+    }
+    else if(text === "c#"){
+        return '<img src="https://img.icons8.com/color/50/000000/c-sharp-logo.png" class="img-fluid">'
+    }
+    else if(text === "github"){
+        return '<img src="https://img.icons8.com/color/50/000000/github.png" class="img-fluid">'
+    }
+    else if(text === "visualstudio"){
+        return '<img src="https://img.icons8.com/color/50/000000/visual-studio.png" class="img-fluid">'
+    }
+}
