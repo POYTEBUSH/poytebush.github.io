e6f6abfc4ed1f6754a4ea3534729014418a9c532
Peter Bush <poytebush@gmail.com>
2018-12-20 23:22:18 +0000

    Added more icons and work

--- iconfinder.js
+++ iconfinder.js
@@ -11,4 +11,14 @@ function GetIcon(text){
     else if(text === "visualstudio"){
         return '<img src="https://img.icons8.com/color/50/000000/visual-studio.png" class="img-fluid">'
     }
+    else if(text === "ai"){
+        return '<img src="https://img.icons8.com/color/48/000000/mind-map.png" class="img-fluid">'
+    }
+    else if(text === "lua"){
+        return '<img src="https://a.fsdn.com/allura/p/luabinaries/icon?1465843094?&w=90" class="img-fluid">'        
+    }
+    else if(text === "groupbased"){
+        return '<img src="https://img.icons8.com/color/48/000000/conference-background-selected.png" class="img-fluid">'
+    }
+    return "";
 }
