b0a6f0173993aa5ca3cfc21ff39978530e7f23d1
Peter Bush <poytebush@gmail.com>
2019-01-10 22:02:59 +0000

    Keep

--- iconfinder.js
+++ iconfinder.js
@@ -1,27 +1,27 @@
 function GetIcon(text){
     if(text === "c++"){
-        return '<img src="https://img.icons8.com/color/50/000000/c-plus-plus-logo.png" class="img-fluid">'
+        return '<img src="https://img.icons8.com/color/50/000000/c-plus-plus-logo.png" class="img-fluid cattag">'
     }
     else if(text === "c#"){
-        return '<img src="https://img.icons8.com/color/50/000000/c-sharp-logo.png" class="img-fluid">'
+        return '<img src="https://img.icons8.com/color/50/000000/c-sharp-logo.png" class="img-fluid cattag">'
     }
     else if(text === "github"){
-        return '<img src="https://img.icons8.com/color/50/000000/github.png" class="img-fluid">'
+        return '<img src="https://img.icons8.com/color/50/000000/github.png" class="img-fluid cattag">'
     }
     else if(text === "visualstudio"){
-        return '<img src="https://img.icons8.com/color/50/000000/visual-studio.png" class="img-fluid">'
+        return '<img src="https://img.icons8.com/color/50/000000/visual-studio.png" class="img-fluid cattag">'
     }
     else if(text === "ai"){
-        return '<img src="https://img.icons8.com/color/48/000000/mind-map.png" class="img-fluid">'
+        return '<img src="https://img.icons8.com/color/48/000000/mind-map.png" class="img-fluid cattag">'
     }
     else if(text === "lua"){
-        return '<img src="https://a.fsdn.com/allura/p/luabinaries/icon?1465843094?&w=90" class="img-fluid">'        
+        return '<img src="https://a.fsdn.com/allura/p/luabinaries/icon?1465843094?&w=90" class="img-fluid cattag">'        
     }
     else if(text === "groupbased"){
-        return '<img src="https://img.icons8.com/color/48/000000/conference-background-selected.png" class="img-fluid">'
+        return '<img src="https://img.icons8.com/color/48/000000/conference-background-selected.png" class="img-fluid cattag">'
     }
     else if(text === "sdl"){
-        return '<img src=" https://i.imgur.com/Ddga03K.png?1" class="img-fluid">'       
+        return '<img src=" https://i.imgur.com/Ddga03K.png?1" class="img-fluid cattag">'       
     }
     
     return "";
